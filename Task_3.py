b522ccb1e8af08ca08e1630beb3b51f434c73a4e
GennaBari <barinovaevgenia84@gmail.com>
2024-09-16 15:51:30 +0300

    Add files via upload

---
+++ Task_3.py
@@ -0,0 +1,4 @@
+print ("3rd program")
+print (1234 % 1000 // 10)
+print (5678 % 1000 // 10)
+print (23 + 67)
---
+++ Task_4.py
@@ -0,0 +1,6 @@
+print ("4th program")
+print ('13.42' , '42.13')
+print (int (13.42), int(42.13))
+print (int((13.42 - 13) * 100), (int((42.13 - 42) * 100)))
+print (13 == 13 and 42 == 42)
+
