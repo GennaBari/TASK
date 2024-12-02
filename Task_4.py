1dcc260eefb1ba4b01748b36420731efc8196f80
GennaBari <barinovaevgenia84@gmail.com>
2024-12-02 12:31:27 +0300

    Update Task_4.py

--- Task_4.py
+++ Task_4.py
@@ -1,6 +1,3 @@
-print ("4th program")
-print ('13.42' , '42.13')
-print (int (13.42), int(42.13))
-print (int((13.42 - 13) * 100), (int((42.13 - 42) * 100)))
-print (13 == 13 and 42 == 42)
+print('4th program:')
+print(int(123.456 * 10 % 10))
 
