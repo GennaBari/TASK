9fbfafa585c4362bf1aa6c692630248c8364bd31
GennaBari <barinovaevgenia84@gmail.com>
2024-12-02 12:33:27 +0300

    Update Task_3.py

--- Task_3.py
+++ Task_3.py
@@ -1,10 +1,5 @@
-print("1st program")
-print(9 ** 0.5 * 5)
-print("2st program")
-print (9.99 > 9.98 and 1000 != 1000.1)
 print("3rd program")
 print (2 * 2 + 2)
 print (2 * (2 + 2))
 print ((2 * 2 + 2) == (2 * (2 + 2)))
-print("4rd program")
-print(int((float('123.456')-int(float('123.456')))*10))
+
